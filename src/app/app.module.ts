28d908f83800888ab7963bb24859f1ddcff1596e
grek-andrian <gretsko.andrian@gmail.com>
2017-03-01 20:22:53 +0200

    Add login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { AngularFireModule } from 'angularfire2';
 import { AppComponent } from './app.component';
+import { AuthService } from './providers/auth.service';
+import { LoginPageComponent } from './login-page/login-page.component'
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyCY6P6Y1vWVqgrpP-srgzMwofAI1NGTteI",
+    authDomain: "ngauth-24551.firebaseapp.com",
+    databaseURL: "https://ngauth-24551.firebaseio.com",
+    storageBucket: "ngauth-24551.appspot.com",
+    messagingSenderId: "28496849633"
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
