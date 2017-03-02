fdff4fbd2663746a5eef9234e1fda0ced7381ba1
grek-andrian <gretsko.andrian@gmail.com>
2017-03-02 16:53:26 +0200

    Add nome-page component and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+
 import { AngularFireModule } from 'angularfire2';
 import { AppComponent } from './app.component';
 import { AuthService } from './providers/auth.service';
-import { LoginPageComponent } from './login-page/login-page.component'
+import { LoginPageComponent } from './login-page/login-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+
 
 export const firebaseConfig = {
     apiKey: "AIzaSyCY6P6Y1vWVqgrpP-srgzMwofAI1NGTteI",
@@ -15,16 +19,23 @@ export const firebaseConfig = {
     messagingSenderId: "28496849633"
 }
 
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'login', component: LoginPageComponent }
+];
+
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    AngularFireModule.initializeApp(firebaseConfig)
+    AngularFireModule.initializeApp(firebaseConfig),
+    RouterModule.forRoot(routes)
   ],
   providers: [AuthService],
   bootstrap: [AppComponent]
